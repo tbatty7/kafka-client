fa78cb0b338b604b5dcd90fecff1a2e487966569
Tim B <tbatty7@gmail.com>
2019-06-03 20:24:54 -0400

    wired up Topics interface to Spring to wire up topic

--- src/main/java/com/example/kafkaclient/DemoApplication.java
+++ src/main/java/com/example/kafkaclient/DemoApplication.java
@@ -2,8 +2,10 @@ package com.example.kafkaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
+@EnableBinding(Topics.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
