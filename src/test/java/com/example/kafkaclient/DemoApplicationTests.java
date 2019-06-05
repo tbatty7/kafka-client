bc4d4412b12c7fc3b8d7fef11411eb1f01222cbe
Tim B <tbatty7@gmail.com>
2019-06-05 09:18:54 -0400

    the test inexplicably broke.  I added this annotation and it worked

--- src/test/java/com/example/kafkaclient/DemoApplicationTests.java
+++ src/test/java/com/example/kafkaclient/DemoApplicationTests.java
@@ -3,9 +3,11 @@ package com.example.kafkaclient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@TestExecutionListeners
 @SpringBootTest
 public class DemoApplicationTests {
 
